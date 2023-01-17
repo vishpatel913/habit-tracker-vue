3cefbf424e349baa12065bf9d9d984232cfff1a0
vishpatel913 <vishpatel913@googlemail.com>
2023-01-17 14:09:28 +0000

    added vant ui library

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,19 @@
 </template>
 
 <style>
+html,
+body {
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: #2c3e50;
+  color: #ffffff;
+  height: 100%;
+  padding: 1rem;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import router from "./router";
 
 import AppLayout from "./layouts/AppLayout.vue";
 
+import "vant/lib/index.css";
+
 const app = createApp(App);
 
 app.use(router);
